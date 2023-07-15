2f571ec8cd16e79e7b8c6053b59f575711ed293a
SIMW23 <yuhdragon23@gmail.com>
2023-07-15 13:33:40 +0700

    more scripts

---
+++ BUV Game Jam 2023/Assets/Scripts/BulletGoal.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletGoal : MonoBehaviour
+{
+    public GameObject mainGoal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Projectile")
+        {
+            Destroy(mainGoal.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
